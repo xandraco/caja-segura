276a600e54813617cc4b5bd7d8414d5ef259e085
xandraco <andraco7x7@outlook.com>
2023-11-15 17:06:42 -0600

    fixing the home for the secure login

--- Backend/Files/home.php
+++ Backend/Files/home.php
@@ -3,15 +3,23 @@
   $conn = conectar();
   $dataPost = file_get_contents('php://input');
   $body = json_decode($dataPost, true);
-  $email = $body['email'];
-  $queryUsuario = "SELECT * FROM users WHERE email = '$email'";
-  $validaUsuario = mysqli_query($conn, $queryUsuario);
 
-  if ($validaUsuario -> num_rows > 0) {
-    $user = $validaUsuario -> fetch_assoc();
-    echo json_encode(['STATUS' => 'SUCCESS', 'MESSAGE' => $user]);
-  } else {
-    echo json_encode(['STATUS' => 'ERROR', 'MESSAGE' => 'No se encontro usuario']);
+  if ($body !== null) {
+    $email = $body['email'];
+
+    // Buscamos el usuario dentro de la base de datos
+    $queryUsuario = "SELECT * FROM users WHERE email = :email";
+    $stmt = $conn->prepare($queryUsuario);
+    $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+    $stmt->execute();
+    $validaUsuario = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if (count($validaUsuario) > 0) {
+      $user = $validaUsuario[0];
+      echo json_encode(['STATUS' => 'SUCCESS', 'MESSAGE' => $user]);
+    } else {
+      echo json_encode(['STATUS' => 'ERROR', 'MESSAGE' => 'No se encontró usuario']);
+    }
   }
 
 ?>
