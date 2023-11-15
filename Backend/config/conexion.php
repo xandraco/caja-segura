cb2527ad7419ab0c7367123479b7af1b4a03e683
Catati64 <catati64@hotmail.com>
2023-11-15 11:55:57 -0600

    Update connect

--- Backend/config/conexion.php
+++ Backend/config/conexion.php
@@ -2,7 +2,7 @@
   function conectar() {
     $host = "https://auth-db531.hstgr.io/";
     $user = "u351541285_SSPS_admin";
-    $pass = ';j$e[Gw7?G';
+    $pass = "HW[CPUs9^Vp";
     $db = "u351541285_ssp_safe";
     $conn = mysqli_connect($host,$user,$pass);
     mysqli_select_db($conn, $db);
