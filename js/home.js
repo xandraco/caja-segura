595b215e2caaa7e439b4aecfe9abf306519d012c
Catati64 <catati64@hotmail.com>
2023-11-18 12:14:06 -0600

    Fix TS P2 1

--- js/home.js
+++ js/home.js
@@ -12,7 +12,7 @@ let timer = 60;
 document.addEventListener('DOMContentLoaded', () => {
     $.ajax({
         type: 'GET',
-        url: './files/session.php',
+        url: './Backend/files/session.php',
         dataType: 'json',
         success: function(response) {
             // Manejar la respuesta del servidor
