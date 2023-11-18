5baec1e9f473e53b6da140a22fdc92d2576e856f
Catati64 <catati64@hotmail.com>
2023-11-18 13:01:51 -0600

    Fix TS p3 8

--- js/home.js
+++ js/home.js
@@ -15,21 +15,21 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 const DataUser = () => {
-    fetch('./Backend/Files/session.php')
-    .then(async response => {
-        const res = await response.json();
-        if (res.STATUS === 'SUCCESS') {
-            // Acceder a los datos de sesión desde la respuesta JSON
-            loggedUser = res.USER;
-            console.log('Usuario: ', loggedUser)
-        } else {
-            console.error('Error:', res.MESSAGE);
-        }
-    }).catch(error => console.error('Error de Fetch:', error));
+    fetch('./Backend/Files/session.php', {method: 'GET'})
+        .then(async response => {
+            const res = await response.json();
+            if (res.STATUS === 'SUCCESS') {
+                // Acceder a los datos de sesión desde la respuesta JSON
+                loggedUser = res.USER;
+                console.log('Usuario: ', loggedUser)
+            } else {
+                console.error('Error:', res.MESSAGE);
+            }
+        }).catch(error => console.error('Error de Fetch:', error));
     loadUser()
 }
 
-const loadUser = () => {    
+const loadUser = () => {
     titulo.innerHTML = loggedUser.id;
     genToken(); // Generar token al inicio
     countDown = setInterval(updateCountDown, 1000);
