dbf9d2ebd2f0cb2fdc2a40a51dd3f5fc487ffa5e
xandraco <andraco7x7@outlook.com>
2023-11-18 12:20:36 -0600

    updated the session

--- Backend/Files/session.php
+++ Backend/Files/session.php
@@ -1,22 +1,10 @@
 <?php
-session_start();
+    session_start();
 
-// Verifica si hay una sesión activa
-if (isset($_SESSION['usuario'])) {
-    // Ya hay una sesión activa, podrías manejarlo de acuerdo a tus necesidades
-    echo json_encode(['STATUS' => 'ERROR', 'MESSAGE' => 'Ya hay una sesión activa']);
-} else {
-    // Recibe los datos del usuario desde la solicitud
-    $dataPost = file_get_contents('php://input');
-    $userData = json_decode($dataPost, true);
-
-    if ($userData !== null && isset($userData['usuario'])) {
-        // Almacena la información del usuario en la sesión
-        $_SESSION['usuario'] = $userData['usuario'];
-
-        echo json_encode(['STATUS' => 'SUCCESS', 'MESSAGE' => 'Sesion validada','USER' => $userData]);
+    if (isset($_SESSION['usuario'])) {
+        $user = $_SESSION['usuario'];
+        echo json_encode(['STATUS' => 'SUCCESS', 'USUARIO' => $user]);
     } else {
-        echo json_encode(['STATUS' => 'ERROR', 'MESSAGE' => 'Datos no válidos']);
+        echo json_encode(['STATUS' => 'ERROR', 'MESSAGE' => 'No hay sesión activa']);
     }
-}
 ?>
