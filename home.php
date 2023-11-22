7092c8e7b9531f1b7a86f32f1bad55d0a04a567f
Catati64 <catati64@hotmail.com>
2023-11-22 10:31:00 -0600

    Admin init

--- home.php
+++ home.php
@@ -26,19 +26,7 @@
     <div class="col-sm-3 sidenav">
       <h4 id="userBlog">Cargando...</h4>
       <ul class="nav nav-pills nav-stacked">
-        <li class="active"><a href="#section1">Home</a></li>
-        <li><a href="#section2">Friends</a></li>
-        <li><a href="#section3">Family</a></li>
-        <li><a href="#section3">Photos</a></li>
-      </ul><br>
-      <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search Blog..">
-        <span class="input-group-btn">
-          <button class="btn btn-default" type="button">
-            <span class="glyphicon glyphicon-search"></span>
-          </button>
-        </span>
-      </div>
+      </ul>
     </div>
 
     <div class="col-sm-9">
